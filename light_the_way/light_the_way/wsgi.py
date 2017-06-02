c429b706e097b5649d5a455009c03598390921ac
Darby Vranek <lyra@Darbys-MacBook-Pro.local>
2017-06-01 18:18:53 -0700

    first commit. initial project is set up

---
+++ light_the_way/light_the_way/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for light_the_way project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "light_the_way.settings")
+
+application = get_wsgi_application()
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/lyra/Documents/hack_for_la/light_the_way/venv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
