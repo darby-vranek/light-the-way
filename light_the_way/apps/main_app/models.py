4ea53f22b70dd0a5ebf91012ced8c100aa932f89
Darby Vranek <lyra@Darbys-MacBook-Pro.local>
2017-06-05 10:39:51 -0700

    second commit

--- light_the_way/apps/main_app/models.py
+++ light_the_way/apps/main_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+	name = models.CharField(max_length=100)
+	category = models.ForeignKey('Category', on_delete=models.CASCADE)
+	date_added = models.DateField(auto_now_add=True)
+	date_updated = models.DateField(auto_now=True)
+
+
+class Category(models.Model):
+	name = models.CharField(max_length=100)
+	date_added = models.DateField(auto_now_add=True)
+	date_updated = models.DateField(auto_now=True)
+
+
+class Resource(models.Model):
+	name = models.CharField(max_length=200)
+	link = models.URLField()
+	description = models.TextField(default='')
+	tags = models.ManyToManyField(Tag)
+	date_added = models.DateField(auto_now_add=True)
+	date_updated = models.DateField(auto_now=True)
--- light_the_way/light_the_way/urls.py
+++ light_the_way/light_the_way/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('apps.main_app.urls'))
+    url(r'^', include('apps.main_app.urls')),
+	url(r'^admin/', admin.site.urls)
 ]
