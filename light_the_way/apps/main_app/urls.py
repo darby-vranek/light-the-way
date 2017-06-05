0dcce0a5f5f1e57fe5d538db8bbc787e523ede03
Darby Vranek <lyra@Darbys-MacBook-Pro.local>
2017-06-05 15:01:39 -0700

    this is a working version that allows multiple tag filtering! the way it is set up, it's possible to bookmark specific queries

--- light_the_way/apps/main_app/urls.py
+++ light_the_way/apps/main_app/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
-	url(r'^select/([0-9]+)/$', views.select)
+    url(r'^$', views.index, name='index'),
+	url(r'^select/(?P<tag_id>[0-9]+)-(?P<active_search>[0-9_]*)/$', views.select),
+	url(r'^show/(?P<tag_ids>[0-9_]*)/$', views.show),
+	url(r'^show/$', views.show_all)
 ]
--- light_the_way/apps/main_app/views.py
+++ light_the_way/apps/main_app/views.py
@@ -1,17 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import sessions
+
 from .models import *
 
 # Create your views here.
-def index(request):
+def index(request, methods=['GET']):
 	context = {
 		'tags': Tag.objects.all(),
 		'resources': Resource.objects.all()
 	}
 	return render(request, 'main_app/index.html', context=context)
 
-def select(request, tag_id):
+def select(request, tag_id='', active_search='', methods=['GET']):
+
+	if len(active_search) > 0:
+		active_ids = active_search.split('_')
+
+		if tag_id in active_ids:
+			active_ids.remove(tag_id)
+		else:
+			active_ids.append(tag_id)
+
+		url_to_show = '_'.join(active_ids)
+
+	else:
+		active_ids = []
+		url_to_show = tag_id
+
+	return redirect('/show/'+url_to_show)
+
+
+def show_all(request):
 	context = {
-		'tags': Tag.objects.all(),
-		'resources': Resource.objects.filter(tags__id=tag_id)
+		'active_search':'',
+		'tags':Tag.objects.all(),
+		'resources':Resource.objects.all()
 	}
-	return render(request, 'main_app/index.html', context=context)
+	return render(request, 'main_app/search.html', context=context)
+
+
+def show(request, tag_ids=''):
+	search_tags = tag_ids.split('_')
+	resources = list()
+	for resource in Resource.objects.all():
+		tags = resource.tags.filter(id__in=[int(tag) for tag in search_tags])
+		if len(tags) > 0:
+			resources.append(resource)
+	context = {
+		'active_search': tag_ids,
+		'tags': Tag.objects.all(),
+		'resources': resources
+	}
+	return render(request, 'main_app/search.html', context=context)
